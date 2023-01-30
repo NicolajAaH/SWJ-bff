b19d0a1018014b3e801230cf83c0dfe4f7872a1d
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-01-30 15:44:16 +0100

    enable CORS

--- src/main/java/dk/sdu/mmmi/backendforfrontend/service/model/Application.java
+++ src/main/java/dk/sdu/mmmi/backendforfrontend/service/model/Application.java
@@ -1,7 +1,14 @@
 package dk.sdu.mmmi.backendforfrontend.service.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@ToString
 public class Application {
     private Long id;
 
--- src/main/java/dk/sdu/mmmi/backendforfrontend/service/model/User.java
+++ src/main/java/dk/sdu/mmmi/backendforfrontend/service/model/User.java
@@ -1,9 +1,15 @@
 package dk.sdu.mmmi.backendforfrontend.service.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class User {
 
     private String email;
-
     private String password;
 
     private String name;
