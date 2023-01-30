eb884151c3e2ebac87436536c8f6c02f29ffc9ef
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-01-30 14:23:38 +0100

    enable CORS

--- src/main/java/dk/sdu/mmmi/backendforfrontend/config/CorsConfig.java
+++ src/main/java/dk/sdu/mmmi/backendforfrontend/config/CorsConfig.java
@@ -2,11 +2,12 @@ package dk.sdu.mmmi.backendforfrontend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Component
 public class CorsConfig{
 
     @Bean
